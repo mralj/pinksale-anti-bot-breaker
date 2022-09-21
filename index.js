7e52de491d471c418f94cd48053f3e64e6c77bed
mralj <nikola.mratinic@gmail.com>
2022-09-21 20:03:46 +0200

    added web3 dependecy

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello, world!');
+var Web3 = require('web3');
+
+const web3Instance = new Web3(
+  new Web3.providers.HttpProvider("https://bsc-dataseed.binance.org/"));
+
+web3Instance.eth.getBalance("0x18Dd66457DF13f08dcf0809cE937aCd5e2d88156").then(console.log);
