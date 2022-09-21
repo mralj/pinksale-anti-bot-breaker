5b0f160e4240d2c5625fe733ad5cf3f604371204
mralj <nikola.mratinic@gmail.com>
2022-09-21 20:21:56 +0200

    listing all memory slots on the test contract

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var Web3 = require('web3');
+var Web3 = require("web3");
 
 const web3Instance = new Web3(
-  new Web3.providers.HttpProvider("https://bsc-dataseed.binance.org/"));
+  new Web3.providers.HttpProvider("https://bsc-dataseed.binance.org/")
+);
 
-web3Instance.eth.getBalance("0x18Dd66457DF13f08dcf0809cE937aCd5e2d88156").then(console.log);
+const countOfStorageSlotsContactHas = 11;
+const contractAddress = "0x0c11c871d1d5091A43778B759E247041E6f08e9A";
+
+getStorageAt().then();
+
+async function getStorageAt() {
+  for (let i = 0; i <= countOfStorageSlotsContactHas; i++) {
+    let slot = await web3Instance.eth.getStorageAt(contractAddress, i);
+    console.log(`${slot} [${i}]`);
+  }
+}
